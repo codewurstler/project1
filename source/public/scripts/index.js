2e73fa0ed489c4bcd3cabcd18f862737dca6c58e
Philipp Isaak <phi.isaak@gmail.com>
2023-05-23 22:50:16 +0200

    more list layout and style toggle

--- source/public/scripts/index.js
+++ source/public/scripts/index.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+// CHANGE STYLE
+document.querySelector("#btn-style").addEventListener("click", changeMode);
+function changeMode() {
+  const element = document.body;
+  element.classList.toggle("dark-mode");
+}
