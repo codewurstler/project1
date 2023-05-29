0a0ea17fb4a74a0e3fcc0069501b371af836ed8d
Philipp Isaak <phi.isaak@gmail.com>
2023-05-29 17:04:16 +0200

    display items from local storage

--- source/public/scripts/index.js
+++ source/public/scripts/index.js
@@ -1,27 +1,3 @@
-window.addEventListener("load", () => {
-  todos = JSON.parse(localStorage.getItem("todos")) || [];
-
-  const todoForm = document.querySelector("#todo-form");
-
-  todoForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const todoItem = {
-      title: document.querySelector("#title").value,
-      description: document.querySelector("#description").value,
-      date: document.querySelector("#date").value,
-      importance: document.querySelector("#importance").value,
-      status: document.querySelector("#status").value,
-    };
-
-    todos.push(todoItem);
-
-    localStorage.setItem("todos", JSON.stringify(todos));
-
-    e.target.reset();
-  });
-});
-
 // CHANGE STYLE
 document.querySelector("#btn-style").addEventListener("click", changeMode);
 function changeMode() {
@@ -47,35 +23,85 @@ document
   .querySelector("#todo-input-overview")
   .addEventListener("click", hideForm);
 
-const toDos = [
-  {
-    title: "Test",
-    description: "Lorem ipsum dolor sit am...",
-    date: "2023-10-10",
-    importance: 1,
-    done: false,
-  },
-  {
-    title: "Test 1",
-    description: "Lorem ipsum dolor sit am...",
-    date: "2023-10-10",
-    importance: 3,
-    done: false,
-  },
-  {
-    title: "Test 2",
-    description: "Lorem ipsum dolor sit am...",
-    date: "2023-11-12",
-    importance: 4,
-    done: true,
-  },
-  {
-    title: "Test 3",
-    description: "Lorem ipsum dolor sit am...",
-    date: "2023-11-09",
-    importance: 2,
-    done: false,
-  },
-];
+window.addEventListener("load", () => {
+  todos = JSON.parse(localStorage.getItem("todos")) || [];
 
-console.log(toDos.title);
+  const todoForm = document.querySelector("#todo-form");
+
+  todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const todoItem = {
+      title: document.querySelector("#title").value,
+      description: document.querySelector("#description").value,
+      date: document.querySelector("#date").value,
+      importance: document.querySelector("#importance").value,
+      status: document.querySelector("#status").value,
+    };
+
+    todos.push(todoItem);
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+    e.target.reset();
+
+    showTodos();
+  });
+  showTodos();
+});
+
+function showTodos() {
+  const showTodoList = document.querySelector(".todo-list");
+  showTodoList.innerHTML = "";
+
+  // CHECKING TODO STATUS
+  todos.forEach((todo) => {
+    function todoStatus() {
+      if (todo.status === "done") {
+        return "Done";
+      } else {
+        return "Open";
+      }
+    }
+    const todoStatusLabel = todoStatus();
+
+    // CHECKING TODO IMPORTANCE
+    function todoImportance() {
+      if (todo.importance === "1") {
+        return "&#9888;";
+      } else if (todo.importance === "2") {
+        return "&#9888; &#9888;";
+      } else if (todo.importance === "3") {
+        return "&#9888; &#9888; &#9888;";
+      } else if (todo.importance === "4") {
+        return "&#9888; &#9888; &#9888; &#9888;";
+      } else if (todo.importance === "5") {
+        return "&#9888; &#9888; &#9888; &#9888; &#9888;";
+      } else {
+        return "No importance selected";
+      }
+    }
+    const todoImportanceLabel = todoImportance();
+
+    const todoItem = document.createElement("div");
+    todoItem.classList.add("todo-list-item");
+    todoItem.innerHTML = `
+      <div class="todo-list-item-inner-1">
+            <p>${todo.date}</p>
+            <input type="checkbox" id="todo-completion" name="todo-completion" value="${todo.status}">
+            <label for="todo-completion">${todoStatusLabel}</label>
+      </div>
+          <div class="todo-list-item-inner-2">
+            <p>${todo.title}</p>
+            <p>${todo.description}</p>
+          </div>
+          <div class="todo-list-item-inner-3">
+            <p>${todoImportanceLabel}</p>
+          </div>
+          <div class="todo-list-item-inner-4">
+            <button id="btn-list-item-edit" class="btn">Edit</button>
+          </div>
+  `;
+    todoList.appendChild(todoItem);
+  });
+}
